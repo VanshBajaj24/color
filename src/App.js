dcf12f701947ae3192632ca310078f5997ee2036
VanshBajaj24 <vansh.bajaj2020@vitstudent.ac.in>
2021-11-10 00:46:38 +0530

    vb

--- src/App.js
+++ src/App.js
@@ -1,66 +1,79 @@
 import logo from './logo.svg';
-import './App.css';
-import randomcolor from './RandomColors'
-import React from 'react';
-
-var ReactDOM = require('react-dom');
-
-class Color extends React.Component {
-  
-  render () {
-    let style = {
-      backgroundColor: this.props.hexCode
-    }
-    return (
-      <div className="color"  style={style}>
-        <div><button 
-  onClick={() =>  navigator.clipboard.writeText(`${this.props.hexCode}`)}
->
-  Copy
-</button></div>
-        <p className="color__code"   ref={(textarea) => this.textArea = textarea} onClick={this.props.update.bind(this, this.props.index)}>{this.props.hexCode}</p>
-      </div>
-    );
-  }
-};
+// import './App.css';
+import RandomColor from './RandomColors'
+import React,{ useState, useEffect }from 'react';
 
 
-class App extends React.Component {
+// var ReactDOM = require('react-dom');
+
+// class Color extends React.Component {
   
-  constructor () {
-    super();
-    this.state = {
-      colorsNum: 5,
-      colors: []
-    };
-    for (let i = 0; i < this.state.colorsNum; i +=1) {
-      this.state.colors.push({hexCode: this.generateColor()});
-    }
-  }
+//   render () {
+    
+//     let style = {
+//       backgroundColor: this.props.hexCode
+//     }
+    
+//     return (
+//       <div className="color"  style={style}>
+       
+//         <button 
+//   onClick={() =>  navigator.clipboard.writeText(`${this.props.hexCode}`)}
+// >
+//   Copy
+// </button>
+//         <p className="color__code"   ref={(textarea) => this.textArea = textarea} >{this.props.hexCode}</p>
+        
+//       </div>
+//     );
+//   }
+// };
+
+
+// class App extends React.Component {
+
+//   constructor () {
+//     super();
+//     this.state = {
+//       colorsNum: 5,
+//       colors: []
+//     };
+//     for (let i = 0; i < this.state.colorsNum; i +=1) {
+//       this.state.colors.push({hexCode: this.generateColor()});
+//     }
+//   }
   
- 
-  generateColor (){
-    return '#' +  Math.random().toString(16).substr(-6);
-  }
+//   generateColor (){
+//     return '#' +  Math.random().toString(16).substr(-6);
+//   }
   
-  updateColor (index) {
-    let colors = this.state.colors.slice();
-    const currentColor = this.generateColor();
-    colors[index].hexCode = currentColor;
-    this.setState({
-      colors : colors
-    });
- }
+//   updateColor (index) {
+//     let colors = this.state.colors.slice();
+//     const currentColor = this.generateColor();
+//     colors[index].hexCode = currentColor;
+   
+//     this.setState({
+//       colors : colors
+//     });
+    
+//  }
   
-  render () {
-    return (
-      <div className="color-container">
-        { this.state.colors.map((color, index) => <Color key={`color-${index}`} index={index} update={this.updateColor.bind(this)} hexCode={color.hexCode}></Color>) }
-      </div>
+//   render () {
+//     return (
+//       <div className="color-container">
+//         { this.state.colors.map((color, index) => <Color key={`color-${index}`} index={index} update={this.updateColor.bind(this)} hexCode={color.hexCode}></Color>) }
+        
+//       </div>
       
-    )
-  }
+//     )
+//   }
+// }
+// ReactDOM.render(<App />, document.getElementById('root'));
+function App(){
+  return(
+    <div>
+      <RandomColor/>
+    </div>
+  )
 }
-ReactDOM.render(<App />, document.getElementById('root'));
-
 export default App;
--- src/RandomColors.js
+++ src/RandomColors.js
@@ -75,3 +75,82 @@
 //   }
   
 //   ReactDOM.render(<App />, document.getElementById('root'));
+import React,{useState ,useEffect} from 'react';
+import ReactDom from 'react-dom';
+import "./App.css"
+
+function RandomColor(){
+   
+    const [color1, setcolor1] = useState(null)
+    const [color2, setcolor2] = useState(null)
+    const [color3, setcolor3] = useState(null)
+    const [color4, setcolor4] = useState(null)
+    const [color5, setcolor5] = useState(null)
+   
+   
+   
+    // if (color[0].includes("#b")==="true"){
+    //        var col1=color[0]
+    // }   else{
+    //  do{ 
+    //      n="#";
+    //     color.splice(0,1)
+    //     for (let i=0;i<=5;i++){
+    //         const randomElement = array[Math.floor(Math.random() * array.length)];
+    //            n=n+randomElement;  
+    //         } 
+    //  while(color[0].includes("#b")==="true");
+     
+     
+   
+
+    const handleKeyDown = (e) => {
+        if(e.keyCode === 32){
+           
+            var array=['a','b','c','d','e','f','0','1','2','3','4','5','6','7','8','9']
+            var color=[];
+            var n ="";
+            for (let m=0;m<=4;m++){
+                n="#"
+            for (let i=0;i<=5;i++){
+                    const randomElement = array[Math.floor(Math.random() * array.length)];
+                       n=n+randomElement;               
+             }
+             color.push(n)
+            }
+
+
+
+            setcolor1(color[1])
+     setcolor2(color[2])
+     setcolor3(color[3])
+     setcolor4(color[4])
+     setcolor5(color[0])
+        }
+      };
+
+
+        useEffect(() => {
+            window.addEventListener("keydown", handleKeyDown);
+          }, []);
+
+
+
+    return(
+        <div className="main" 
+        tabIndex="0" >
+            <div className="d1" style={{backgroundColor:color1}}  onClick={() => { navigator.clipboard.writeText(color1)
+             alert("copied")}}><p>{color1}</p></div>
+            <div className="d2" style={{backgroundColor:color2}} onClick={() => { navigator.clipboard.writeText(color2)
+            alert("copied")}}> <p>{color2}</p></div>
+            <div className="d3" style={{backgroundColor:color3}} onClick={() =>  {navigator.clipboard.writeText(color3)
+            alert("copied")}}><p>{color3}</p></div>
+            <div className="d4" style={{backgroundColor:color4}} onClick={() =>  {navigator.clipboard.writeText(color4)
+            alert("copied")}}><p>{color4}</p></div>
+            <div className="d5" style={{backgroundColor:color5}} onClick={() =>  {navigator.clipboard.writeText(color5)
+            alert("copied")}}><p>{color5}</p></div>
+           
+        </div>
+    )
+}
+export default RandomColor ;
